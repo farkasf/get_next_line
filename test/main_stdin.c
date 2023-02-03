1d954e76b16550935824cc3c92295c5ae2b97cf6
farkasf <120285330+farkasf@users.noreply.github.com>
2023-02-03 14:11:01 +0100

    Added stdin test and 42 headers.

--- test/main_stdin.c
+++ test/main_stdin.c
@@ -1,43 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*   main_stdin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/03 14:09:58 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/02/03 14:10:00 by ffarkas          ###   ########.fr       */
+/*   Created: 2023/02/03 14:09:50 by ffarkas           #+#    #+#             */
+/*   Updated: 2023/02/03 14:09:52 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
 
-int	main(int ac, char **av)
+int	main()
 {
-	int fd1, fd2, fd3;
+	char *line;
 	int	i;
-	char	*s;
-	
-	fd1 = open(av[1], O_RDONLY);
-	fd2 = open(av[2], O_RDONLY);
-	fd3 = open(av[3], O_RDONLY);
-	if (fd1 == -1 || fd2 == -1 || fd3 == -1)
-		return 0;
+
 	i = 0;
-	while (i < 4)
+	while ((line = get_next_line(0)))
 	{
-		s = get_next_line(fd1);
-		printf("line %d: %s", i, s);
-		s = get_next_line(fd2);
-		printf("line %d: %s", i, s);
-		s = get_next_line(fd3);
-		printf("line %d: %s", i, s);
+		printf("line %d: %s", i, line);
+		free(line);
 		i++;
 	}
-	close(fd1);
-	close(fd2);
-	close(fd3);
 	return (0);
 }
